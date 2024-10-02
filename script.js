8be851a1760c8b5664e075b7d5749aee314c504c
mizuki <0zf12342787019w@au.com>
2024-10-02 10:25:51 +0900

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("reservation-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("予約が完了しました！");
+});
+
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("ログインしました！");
+});
